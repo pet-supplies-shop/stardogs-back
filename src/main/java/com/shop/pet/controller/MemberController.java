319b50da6586fa24b606b25e19de6d469c80d285
Hanyeojun <hyj78396@naver.com>
2025-03-19 02:15:22 +0900

    토큰 로그인 확인

--- src/main/java/com/shop/pet/controller/MemberController.java
+++ src/main/java/com/shop/pet/controller/MemberController.java
@@ -15,33 +15,37 @@ import com.shop.pet.dto.Member;
 import com.shop.pet.service.MemberService;
 
 
-@RestController
-@CrossOrigin("http://127.0.0.1:5500/")
+@RestController //json 데이터 반환
+@CrossOrigin("http://127.0.0.1:5500/") //CORS 요청 허용
 public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
 	
+	//로그아웃
 	@PostMapping("logout")
 	public void logout(@RequestHeader String authorization) { //request 헤더로 들어옴
 		System.out.println(authorization);
 		try {
+			//토큰 삭제
 			memberService.logout(authorization);
+			System.out.println("토큰 삭제");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	//토큰 로그인
 	@PostMapping("tokenLogin")
 	public Map<String,String> tokenLogin(@RequestBody Member m) {
-		System.out.println(m);
+		//System.out.println(m);
 		
-		Map<String,String> responseMap=new HashMap<>();
+		Map<String,String> responseMap = new HashMap<>();
 		
 		try { //loginInfo에 email, token, name 들어가 있음
 			Login loginInfo = memberService.tokenLogin(m);
 			
+			//로그인 성공 시 이름, 토큰 반환
 			if(loginInfo!=null && loginInfo.getName()!=null && loginInfo.getToken()!=null) {
 				responseMap.put("name", loginInfo.getName());
 				responseMap.put("Authorization", loginInfo.getToken());
@@ -50,20 +54,23 @@ public class MemberController {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			responseMap.put("msg", "다시 로그인 해주세요");
+			responseMap.put("msg", "exception!");
 		}
 		return responseMap;
 	}
 	
+	//일반 로그인
 	@PostMapping("login")
 	public Map<String,String> login(@RequestBody Member m) {
-		System.out.println(m);
+		//System.out.println(m);
 		
 		Map<String,String> responseMap=new HashMap<>();
 		
 		try {
-			m=memberService.login(m);
-			String name=m.getName();
+			m = memberService.login(m);
+			String name = m.getName();
+			
+			//로그인 성공 시 이름 반환
 			if(m!=null && name!=null && !name.trim().equals("")) {
 				responseMap.put("name", name);
 			}else {
@@ -71,23 +78,26 @@ public class MemberController {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			responseMap.put("msg", "다시 로그인 해주세요");
+			responseMap.put("msg", "Exception!");
 		}
 		return responseMap;
 	}
 	
+	//회원가입
 	@PostMapping("insertMember")
 	public String insertMember(@RequestBody Member m) {
-		System.out.println(m);
+		//System.out.println(m);
+		
 		try {
 			memberService.insertMember(m);
-			return m.getName()+"님 가입을 환영합니다";
+			return m.getName()+"님 회원가입 되었습니다.";
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "회원 가입 실패";
 		}
 	}
 	
+	//회원 정보 수정
 	@PostMapping("updateMember")
 	public String updateMember(@RequestBody Member m) {
 		System.out.println(m);
@@ -96,20 +106,20 @@ public class MemberController {
 			return "ok";
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "email과 pwd 확인해 주세요";
+			return "email과 pwd를 확인해 주세요";
 		}
 	}
 	
+	//회원 삭제
 	@PostMapping("deleteMember")
 	public String deleteMember(@RequestBody String email) {
-		System.out.println(email);
+		System.out.println("삭제 요청된 email" + email);
 		try {
 			memberService.deleteMember(email);
-			return "ok";
+			return email + "회원 삭제됨";
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "email과 pwd 확인해 주세요";
+			return "email과 pwd를 확인해 주세요";
 		}
 	}
-
 }
--- src/main/java/com/shop/pet/service/MemberService.java
+++ src/main/java/com/shop/pet/service/MemberService.java
@@ -15,31 +15,32 @@ import com.shop.pet.util.OpenCrypt;
 @Service
 public class MemberService {
 	@Autowired
-	MemberDao memberDao;
+	MemberDao memberDao; //회원 관련 DB 작업
 	
 	@Autowired
-	LoginDao loginDao;
+	LoginDao loginDao; //로그인 관련 DB 작업
 
 	public Login checkToken(String authorization) throws Exception {
+		 //토큰이 DB에 존재하는지 확인
 		return loginDao.checkToken(authorization);
 	}
 	
 	public Login tokenLogin(Member m) throws Exception {
-		m = memberDao.login(m);
+		m = memberDao.login(m); //이메일, 비번 일치하는 회원 정보 조회
 		if(m!=null) {
-			String name=m.getName();
+			String name = m.getName();
 			if(name!=null && !name.trim().equals("")) {
 				
 				String email=m.getEmail();
 				
-				String salt=UUID.randomUUID().toString();
-				System.out.println("salt:"+salt);
+				String salt=UUID.randomUUID().toString(); //난수 문자열 salt 생성
+				System.out.println("salt:"+salt); //salt 확인
 
 				byte[] originalHash=OpenCrypt.getSHA256(email, salt);
 				String myToken=OpenCrypt.byteArrayToHex(originalHash);
 				System.out.println("myToken : "+myToken);
 				
-				Login loginInfo=new Login(email, myToken, name, new Date());
+				Login loginInfo = new Login(email, myToken, name, new Date());
 				loginDao.insertToken(loginInfo);
 				return loginInfo;
 			}
@@ -48,25 +49,31 @@ public class MemberService {
 		return null;		 
 	}
 	
+	//회원 로그인
 	public Member login(Member m) throws Exception {
 		return memberDao.login(m);
 	}
 	
+	//회원가입
 	public void insertMember(Member m) throws Exception{
 		memberDao.insertMember(m);
 	}
 	
+	//회원 정보 수정
 	public void updateMember(Member m) throws Exception{
 		memberDao.updateMember(m);
 	}
 	
+	//회원 삭제
 	public void deleteMember(String email) throws Exception{
+		//System.out.println("실행");
 		memberDao.deleteMember(email);
 	}
 	
+	//회원 로그아웃
 	public void logout(String authorization) throws Exception {
+		//토큰을 받아서 DB에서 해당 토큰 삭제
 		loginDao.deleteToken(authorization);
-		
 	}
 
 }
